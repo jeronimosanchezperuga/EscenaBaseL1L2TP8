a1acb9e5475fc3872d48c3d25ed04379a5f193bc
FacuSalco <facusalco26@gmail.com>
2022-09-12 14:34:00 -0300

    Minor Commit

---
+++ Assets/Scripts/Algo/Crosshair.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crosshair : MonoBehaviour
+{
+    public Image mira;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mira.color = Color.grey;
+            Debug.Log("Click");
+            mira.transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            mira.color = Color.white;
+            Debug.Log("Saco Click");
+            mira.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+        }
+    }
+}
